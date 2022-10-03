4f2706c02d43442fa5c26664f2a6095cf4a4c4fc
AncizarTorres <ancizar_19_99.com>
2022-10-02 20:57:45 -0500

    ajust

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@mui/material/Grid';
+import BasicCard from "./components/Card/BasicCard";
+import BasicTable from "./components/Table/BasicTable";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2}  justifyContent="center">
+      <Grid item xs={5}>
+        <BasicCard title='Solicitados'>
+          <BasicTable />
+        </BasicCard>
+      </Grid>
+      <Grid item xs={5}>
+        <BasicCard title='Asignados'>
+          <BasicTable />
+        </BasicCard>
+      </Grid>
+      <Grid item xs={4}>
+        xs=4
+      </Grid>
+      <Grid item xs={8}>
+        xs=8
+      </Grid>
+    </Grid>
   );
 }
 
