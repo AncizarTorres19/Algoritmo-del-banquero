f36f8956a5dabac7168f114dbfd0e0babfa33d7a
AncizarTorres <ancizar_19_99.com>
2022-10-03 01:15:12 -0500

    ajust tables css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
 import Grid from '@mui/material/Grid';
 import BasicCard from "./components/Card/BasicCard";
+import { columnsA, columnsC, columnsCA, columnsDisponibles, columnsMax, rowsA, rowsC, rowsCA, rowsDisponibles, rowsMax } from './components/Data/DataTable';
 import BasicTable from "./components/Table/BasicTable";
 
 function App() {
   return (
-    <Grid container spacing={2}  justifyContent="center">
-      <Grid item xs={5}>
-        <BasicCard title='Solicitados'>
-          <BasicTable />
-        </BasicCard>
+    <>
+      <Grid container
+        spacing={2}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-evenly"
+        sx={{ maxWidth: '100%', p: 1 }}
+      >
+        <Grid item xs={5}>
+          <BasicCard title='Solicitados'>
+            <BasicTable
+              columns={columnsC}
+              rows={rowsC}
+            />
+          </BasicCard>
+        </Grid>
+        <Grid item xs={5}>
+          <BasicCard title='Asignados'>
+            <BasicTable
+              columns={columnsA}
+              rows={rowsA}
+            />
+          </BasicCard>
+        </Grid>
       </Grid>
-      <Grid item xs={5}>
-        <BasicCard title='Asignados'>
-          <BasicTable />
-        </BasicCard>
+      <Grid container
+        spacing={2}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-evenly"
+        sx={{ maxWidth: '100%', p: 1 }}
+      >
+        <Grid item xs={3}>
+          <BasicCard title='Disponibles'>
+            <BasicTable
+              columns={columnsDisponibles}
+              rows={rowsDisponibles}
+            />
+          </BasicCard>
+        </Grid>
+        <Grid item xs={5}>
+          <BasicCard title='Respuesta'>
+            <BasicTable
+              columns={columnsCA}
+              rows={rowsCA}
+            />
+          </BasicCard>
+        </Grid>
+        <Grid item xs={3}>
+          <BasicCard title='Máximo'>
+            <BasicTable
+              columns={columnsMax}
+              rows={rowsMax}
+            />
+          </BasicCard>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        xs=4
-      </Grid>
-      <Grid item xs={8}>
-        xs=8
-      </Grid>
-    </Grid>
+    </>
   );
 }
 
---
+++ src/components/Data/DataTable.js
@@ -0,0 +1,55 @@
+export const columnsC = [
+    { field: 'R0', headerName: 'C', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+    { field: 'R1', headerName: 'R1', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+    { field: 'R2', headerName: 'R2', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+    { field: 'R3', headerName: 'R3', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+];
+export const rowsC = [
+    { id: 1, R0: 'P1', R1: '3', R2: '2', R3: '2', },
+    { id: 2, R0: 'P2', R1: '6', R2: '1', R3: '3', },
+    { id: 3, R0: 'P3', R1: '3', R2: '1', R3: '4', },
+    { id: 4, R0: 'P4', R1: '4', R2: '2', R3: '2', },
+];
+
+export const columnsA = [
+    { field: 'R0', headerName: 'A', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+    { field: 'R1', headerName: 'R1', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+    { field: 'R2', headerName: 'R2', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+    { field: 'R3', headerName: 'R3', flex: 1, editable: true, headerClassName: 'super-app-theme--header' },
+];
+export const rowsA = [
+    { id: 1, R0: 'P1', R1: '1', R2: '0', R3: '0' },
+    { id: 2, R0: 'P2', R1: '6', R2: '1', R3: '3' },
+    { id: 3, R0: 'P3', R1: '2', R2: '1', R3: '1' },
+    { id: 4, R0: 'P4', R1: '0', R2: '0', R3: '2' },
+];
+
+export const columnsCA = [
+    { field: 'R0', headerName: 'C-A', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'R1', headerName: 'R1', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'R2', headerName: 'R2', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'R3', headerName: 'R3', flex: 1, headerClassName: 'super-app-theme--header' },
+];
+export const rowsCA = [
+    { id: 1, R0: 'P1', R1: '2', R2: '2', R3: '2' },
+    { id: 2, R0: 'P2', R1: '6', R2: '0', R3: '0' },
+    { id: 3, R0: 'P3', R1: '1', R2: '0', R3: '3' },
+    { id: 4, R0: 'P4', R1: '4', R2: '2', R3: '0' },
+];
+
+export const columnsDisponibles = [
+    { field: 'R1', headerName: 'R1', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'R2', headerName: 'R2', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'R3', headerName: 'R3', flex: 1, headerClassName: 'super-app-theme--header' },
+];
+export const rowsDisponibles = [
+    { id: 1, R1: '9', R2: '2', R3: '6' },
+];
+export const columnsMax = [
+    { field: 'R1', headerName: 'R1', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'R2', headerName: 'R2', flex: 1, headerClassName: 'super-app-theme--header' },
+    { field: 'R3', headerName: 'R3', flex: 1, headerClassName: 'super-app-theme--header' },
+];
+export const rowsMax = [
+    { id: 1, R1: '6', R2: '1', R3: '0' },
+];
